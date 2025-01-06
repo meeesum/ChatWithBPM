80cc548e1ae420c0bf6d25ddbbbf54208d349da3
ASAD ALI AAiSH <bcsf21m.pucit@gmail.com>
2025-01-06 16:55:43 +0500

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { FaEnvelope, FaKey, FaGoogle, FaFacebook } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
-import DataProtectionImage from 'C:\\Users\\Meesum Raza\\Desktop\\ChatWithBPM\\ChatWithBPM\\src\\assets\\images\\data_protection.jpg';
-import LogoImage from 'C:\\Users\\Meesum Raza\\Desktop\\ChatWithBPM\\ChatWithBPM\\src\\assets\\images\\Logo.png';
+import DataProtectionImage from '..\assets\images\data_protection.jpg';
+import LogoImage from '..\assets\images\Logo.png';
 
 const LoginPage = () => {
   // States for form inputs
